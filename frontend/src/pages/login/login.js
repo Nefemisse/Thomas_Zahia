824b8ffd2a2eda8f93abe16c17df17144af24193
zahia <zahobouira@gmail.com>
2022-08-01 23:54:06 +0200

    creation frontend

---
+++ frontend/src/pages/login/login.js
@@ -0,0 +1,17 @@
+import".login.css"
+export default  function login(){
+ return (
+    <dev className ="login">
+        <dev className="loginwrapper">
+            <dev className="loginleft">
+                 <h3>Groupomania</h3>
+
+            </dev>
+            <dev className="loginright">
+            </dev>
+        </dev>
+
+
+    </dev>
+ )
+}
