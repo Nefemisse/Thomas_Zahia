d827bcf5b95cce0236f463cc62d88c35b1e93a13
zahia <zahobouira@gmail.com>
2022-08-02 23:36:38 +0200

    creation home page

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -8,25 +8,25 @@ export default function Post() {
             <div className="postTop">
                 <div className="postTopLeft">
                     <img className="postProfileImg" src="assets/images/img9.jpg" alt=""/>
-                    <span className="postUsername"> name</span>
+                    <span className="postUsername">Maya</span>
                     <span className="postDate">5 jours</span>
                 </div>
-                <div className="postTopRight"></div>
             </div>
             <div className="postCenter">
                 <span className="postText">description</span>
-                <img className="postImg" src="assets/images/img5.jpg" alt="" />
+                <img className="postImg" src="assets/images/img1.jpg" alt="" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img className="likeIcon" src="assets/heart.png"  alt="" />
-                    <span className="postLikeCounter"> people like it</span>
+                    <span className="postLikeCounter"> 3 people like it</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText"> comments</span>
+                    <span className="postCommentText"> 9comments</span>
                 </div>
             </div>
         </div>
     </div>
+    
   );
 }
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,7 +1,6 @@
 
 import Topbar from "../../components/topbar/Topbar";
 import Feed from "../../components/feed/Feed";
-import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.css";
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
         <Topbar />
             <div className="homeContainer">
                 <Feed/>
-                <Rightbar/>
             </div>
     </>
   )
