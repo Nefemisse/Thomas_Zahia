bad1cfe78c1af1b59dd164797a22efc284dde4e4
zahia <zahobouira@gmail.com>
2022-08-02 18:11:43 +0200

    page login-register and profile

---
+++ frontend/src/components/rightbar/Rightbar.js
@@ -0,0 +1,19 @@
+import "./rightbar.css";
+export default function Rightbar() {
+    
+      return (
+        <>
+          <div className="birthdayContainer">
+            
+            <span className="birthdayText">
+              <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+            </span>
+          </div>
+          
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            
+          </ul>
+        </>
+      );
+    };
